8accd073afdd203981f750e1dd2782cbee75f089
AlexandraJuly <catmon0201@gmail.com>
2023-03-17 12:29:17 +0200

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+(function() {
+    const input = document.querySelector('input');
+    const box = document.querySelector('div');
+
+
+    input.addEventListener('click', function () {
+        box.classList.add('active')
+    });
+
+document.addEventListener('mouseup', function(evt) {
+    if (input !== document.activeElement && box !== evt.target) {
+        box.classList.remove('active')
+    };
+})
+})();
